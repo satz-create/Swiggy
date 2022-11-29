8607b0bc0588c6c96f37851729bbeb995e02c31b
satz-create <thesathishkumar05@gmail.com>
2022-11-29 20:38:55 +0530

    message

---
+++ src/test/java/org/test/GITclass/Sample1.java
@@ -0,0 +1,22 @@
+package org.test.GITclass;
+
+public class Sample1 {
+	
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method4() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
